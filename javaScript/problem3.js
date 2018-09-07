ea93c2bb2c9d46f25df87df1537039d2ee8a9b8b
Christian Johannes <cajiky@gmail.com>
2018-09-07 09:28:43 -0500

    problem 3 solved

---
+++ javaScript/problem3.js
@@ -0,0 +1,7 @@
+//Your task is to drive your car from 1 to 60 Kilometers per hour by printing a pattern.
+//A hyphen represents the speed of cars.
+//one hyphen is counted as 1 kph.
+
+function Go(num) {
+	return '-'.return(num);
+}
