c174bccccf60fb5277c4c066c1ef28e0a8ae00ef
Christian Johannes <cajiky@gmail.com>
2018-09-07 08:23:29 -0500

    problem 1 done

---
+++ javaScript/problem1.js
@@ -0,0 +1,14 @@
+// create a function that takes a number as its only argument and returns true if
+// it's less than or equal to zero, otherwise return false.
+
+
+function lessThanOrEqualToZero(num) {
+if(num <= 0){
+  return true;
+}
+else {
+  return false;
+}
+}
+
+console.log(lessThanOrEqualToZero(-2))
